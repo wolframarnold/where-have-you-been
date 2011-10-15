1ad7c41a94c75eaeaafb35d359d3d144ef19ebd1
Wolfram Arnold <wolfram@rubyfocus.biz>
2011-10-15 15:54:52 -0700

    Initial commit with boubon, blueprint and haml installed (for styling) Home#index controller action with image for checking layout and style.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhereHaveYouBeen::Application.config.secret_token = '0ad2dc95e52305708f0b28892811ba1cd9330b14a678b33127cd798608041854b859d72a2e334bba4eeab9e1e0e9f4f5395c6dab8ab0b64e54639c3801a83ec5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WhereHaveYouBeen::Application.config.session_store :cookie_store, key: '_where_have_you_been_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WhereHaveYouBeen::Application.config.session_store :active_record_store
