d46d3bc547286e304279c80b9508164a17adb12b
Wolfram Arnold <wolfram@rubyfocus.biz>
2011-11-08 10:12:42 -0800

    Scaffold-generated Trips. Added validation requirement for name, user in Trip model. Controller specs now failing because of Trip business requirement. Views not yet corrected.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,4 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-
-  has_many :trips, :dependent => :destroy
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,4 @@
 require 'spec_helper'
 
 describe User do
-  context 'Association' do
-    it 'responds to trips' do
-      should respond_to(:trips)
-    end
-    it 'can create a trip' do
-      user = FactoryGirl.create(:user)
-      expect {
-        user.trips.create(:name => 'Hello World!')
-      }.should change(user.trips,:count).by(1)
-    end
-    it 'will remove dependents' do
-      trip = FactoryGirl.create(:trip)
-      user = trip.user
-      expect {
-        user.destroy
-      }.to change(Trip,:count).by(-1)
-    end
-  end
 end
