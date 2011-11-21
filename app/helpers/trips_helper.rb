1747dda3038ccf71492ee3e9699873fd66101922
Wolfram Arnold <wolfram@rubyfocus.biz>
2011-11-21 09:12:04 -0800

    Augmented show action to display places as markers on map and polylines between them. No tests yet.

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,2 +1,15 @@
 module TripsHelper
+
+  def geocoded
+    @geocoded ||= @trip.places.inject([]) do |array,place|
+      array << Gmaps4rails.geocode(place.name).first
+    end
+  end
+
+  def polylines
+    polylines = geocoded.map do |geo|
+      {lng: geo[:lng], lat: geo[:lat]}
+    end
+    [polylines]  # the API expects and array of polylines
+  end
 end
